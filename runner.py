b64e7cf5958a2cb44ee69e77ecd87f57f2536915
Shrihari <shariethernet@gmail.com>
2023-01-01 20:36:54 +0530

    Edits

--- runner.py
+++ runner.py
@@ -1,18 +1,21 @@
 from edalize import *
 import os
-work_root = 'build_tlv5'
+work_root = 'build_multifile'
 
 files = [
-    {'name':os.path.relpath('1_adder.tlv',work_root),'file_type':'TLverilogSource'}
+    {'name':os.path.relpath('1_adder.tlv',work_root),'file_type':'TLVerilogSource'}
 ]
 
 tool_options = {
-		"sandpipersaas": {
+		"sandpipersaas": {                  # Any arguments that needed to be passed to sandpiper-saas
 			"sandpiper_saas": [
-				"--bestsv"
+				"--bestsv", "--inlineGen"
 			],
-            "output_file":"1_adder_rtl.sv",
-            "output_dir":"out1"
+            "output_file":"1_adder_rtl.sv", # One file name that ends with .v or .sv
+            "output_dir":"out1"  #,           # Optional
+            # "sandpiper_jar":" <Arguments to the sandpiper compiler>",
+            # "endpoint":"<Optional: URL for the compile service endpoint",
+            # "includes" :"List of include files to be used durung compilation "
 		}
 	}
 tool = 'sandpipersaas'
